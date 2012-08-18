27dd3bc2a87896c9188ae6ef9aaaa35d0617e8a7
Etienne Millon <me@emillon.org>
2012-08-18 16:15:47 +0200

    Remove useless files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(stepmania):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="stepmania" command="/usr/bin/stepmania"
--- debian/stepmania.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the stepmania package
-#
-0 4	* * *	root	[ -x /usr/bin/stepmania_maintenance ] && /usr/bin/stepmania_maintenance
